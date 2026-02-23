42c6a4ee2fc2f095dc7f2f3a519d84ecca02769e
AugustoAFS <augusto.farias.santo03@gmail.com>
2026-02-23 19:32:48 -0300

    add: refatoring for building deploy

--- api/src/main/java/com/AchadosPerdidos/API/ApiApplication.java
+++ api/src/main/java/com/AchadosPerdidos/API/ApiApplication.java
@@ -3,12 +3,28 @@ package com.AchadosPerdidos.API;
 import com.AchadosPerdidos.API.Application.Config.EnvironmentConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(
+		basePackages = "com.AchadosPerdidos.API.Domain.Repository",
+		excludeFilters = @org.springframework.context.annotation.ComponentScan.Filter(
+				type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE,
+				classes = com.AchadosPerdidos.API.Domain.Repository.ChatMessageRepository.class
+		)
+)
+@EnableMongoRepositories(
+		basePackages = "com.AchadosPerdidos.API.Domain.Repository",
+		includeFilters = @org.springframework.context.annotation.ComponentScan.Filter(
+				type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE,
+				classes = com.AchadosPerdidos.API.Domain.Repository.ChatMessageRepository.class
+		)
+)
 public class ApiApplication {
 
 	public static void main(String[] args) {
